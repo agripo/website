58c61dca8ac526648c07f5d476824e09d4ead537
Brice PARENT <briceparent@free.fr>
2015-08-06 05:58:49 +0200

    FT now verifies that autoconnect and persona connect are equivalent

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -92,15 +92,15 @@ class FunctionalTest(StaticLiveServerTestCase):
             )
         )
 
-    def wait_to_be_logged_in(self, email):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(email, navbar.text)
+    def logout(self):
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
 
-    def wait_to_be_logged_out(self, email):
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+
+    def wait_to_be_logged_out(self):
         self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(email, navbar.text)
 
     def wait_for(self, function_with_assertion, timeout=DEFAULT_WAIT):
         start_time = time.time()
--- functional_tests/test_layout_styling_autoconnexion.py
+++ functional_tests/test_layout_styling_autoconnexion.py
@@ -5,9 +5,10 @@ import time
 
 class LayoutAndStylingTest(FunctionalTest):
 
-    def test_can_autoconnect(self):
+    def test_can_autoconnect_and_connect_with_persona(self):
         # Alpha goes to the home page
         self.show_page("", 1)
+        window_handle = self.browser.current_window_handle
 
         # He sees he's not connected
         self.browser.find_element_by_id('id_login')
@@ -16,10 +17,48 @@ class LayoutAndStylingTest(FunctionalTest):
         self.create_autoconnected_session("alpha@mail.com")
 
         # He goes back to the home page to verify he is connected
-        self.show_page("", 1)
+        self.show_page("")
+        content_autoconnected = self.browser.find_element_by_css_selector('body').text
 
-        # He sees he is
-        self.browser.find_element_by_id('id_logout')
+        # He sees he is, and he disconnects
+        self.logout()
+
+        # He now sees he is disconnected
+        connect_button = self.browser.find_element_by_id('id_login')
+
+        # He connects through persona
+        connect_button.click()
+
+        # A Persona login box appears. He switches to it
+        ## Getting the last opened window
+        last_handle = ''
+        for temp_handle in self.browser.window_handles:
+            last_handle = temp_handle
+
+        self.browser.switch_to.window(last_handle)
+
+        # He logs in with his other email address
+        ## Uses mockmyid.com for test email
+        self.browser.find_element_by_id('authentication_email').send_keys('alpha@mockmyid.com')
+        self.browser.find_element_by_tag_name('button').click()
+
+        # The Persona window closes
+        self.browser.switch_to.window(window_handle)
+
+        # He goes back to the home page to verify he is connected again
+        self.wait_to_be_logged_in()
+        self.show_page("")
+        content_with_persona = self.browser.find_element_by_css_selector('body').text
+
+        # He confirms that the content of the page is the same as with the other login
+        self.assertEqual(
+            content_autoconnected.replace('alpha@mail.com', '[EMAIL]'),
+            content_with_persona.replace('alpha@mockmyid.com', '[EMAIL]'))
+
+        # Refreshing the page, he sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
 
     def _test_centering_for_width(self, width):
         self.browser.set_window_size(width, 1000)
