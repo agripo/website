20996772ca2d9d26cced5651c0432ffc6c2d72f4
Brice PARENT <briceparent@free.fr>
2015-08-07 14:19:39 +0200

    Simplification of assertions

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -13,11 +13,13 @@ class AnyViewTest(TestCase):
 
     def test_cookie_notification_shown_variable_in_context_at_startup(self):
         response = self.client.get(reverse('home_page'))
-        if response.context['cookies_notification_shown']:
-            self.fail("The context variable cookies_notification_shown should be False at startup")
+        self.assertFalse(
+            response.context['cookies_notification_shown'],
+            "The context variable cookies_notification_shown should be False at startup")
 
     def test_cookie_notification_shown_variable_in_context_after_acceptation(self):
         self.client.get(reverse('using_cookies_accepted'))
         response = self.client.get(reverse('home_page'))
-        if not response.context['cookies_notification_shown']:
-            self.fail("The context variable cookies_notification_shown should be True once accepted")
+        self.assertTrue(
+            response.context['cookies_notification_shown'],
+            "The context variable cookies_notification_shown should be True once accepted")
