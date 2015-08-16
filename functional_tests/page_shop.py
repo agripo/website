5969362c201e568b265bf9acdd30e939c9872670
Brice PARENT <briceparent@free.fr>
2015-08-16 18:13:10 +0200

    Creation of the shop functionalities

--- functional_tests/page_shop.py
+++ functional_tests/page_shop.py
@@ -1,23 +1,16 @@
 from django.core.urlresolvers import reverse
 
 
-class NewsPage(object):
-    id_page = "id_news_list_page"
-    id_news_list_container = "id_news_list_container"
-    id_field_title = 'id_title'
-    id_field_publication_date_date = 'id_publication_date_0'
-    id_field_publication_date_time = 'id_publication_date_1'
-    id_field_content = 'id_content'
-    id_field_writer = 'id_writer'
-    id_news_title = 'id_news_title'
+class ShopPage(object):
+    id_page = "id_shop_page"
 
     def __init__(self, test):
         self.test = test
 
     def show(self):
-        self.test.browser.get(self.test.server_url+reverse("news_page"))
-        self.test.wait_for(self._is_news_page)
+        self.test.browser.get(self.test.server_url+reverse("shop_page"))
+        self.test.wait_for(self._is_shop_page)
         return self
 
-    def _is_news_page(self):
+    def _is_shop_page(self):
         return self.test.browser.find_element_by_id(self.id_page)
