c637d4b6925c43aaaa28af97c27e685f09ebfe5f
Brice PARENT <briceparent@free.fr>
2015-08-30 14:16:45 +0200

    Added many FT and todos

---
+++ functional_tests/rich_text_editor.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.action_chains import ActionChains
+
+class RichTextEditor(object):
+
+    def __init__(self, test, field_id):
+        self.test = test
+        self.browser = test.browser
+        self.field_id = field_id
+        self.editor_frame = self.browser.find_element_by_css_selector(
+            'iframe[title="Rich Text Editor, {}"]'.format(self.field_id))
+
+    def _activate(self, frame=None):
+        if not frame:
+            frame = self.editor_frame
+        self.browser.switch_to.frame(frame)
+
+    def _deactivate(self):
+        self.browser.switch_to.default_content()
+
+    def _get_content_element(self):
+        return self.browser.find_element_by_css_selector("body")
+
+    def insert_content(self, content):
+        self._activate()
+        self.browser.find_element_by_css_selector("body").send_keys(content)
+        self._deactivate()
