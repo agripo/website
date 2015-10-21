22a29b4f03bf39629b27c90ce07f191497075efe
Brice PARENT <briceparent@free.fr>
2015-10-21 10:26:30 +0200

    Added support for caching fixed blocks

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from core.authentication import is_staging_server, is_development_server
 from django.conf.urls import url, patterns
 from django.contrib.auth.views import logout
 from django.views.generic import RedirectView, TemplateView
+from django.views.decorators.cache import never_cache
 
 from core import views
 
@@ -12,14 +13,14 @@ urlpatterns = [
     url(r'^using_cookies_accepted/$', views.using_cookies_accepted, name="using_cookies_accepted"),
     url(r'^news/$', views.NewsListPage.as_view(), name="news_page"),
     url(r'^shop/$', views.ShopPage.as_view(), name="shop_page"),
-    url(r'^shop/checkout$', views.Checkout.as_view(), name="checkout"),
-    url(r'^shop/get_cart$', views.get_cart, name="get_cart"),
-    url(r'^shop/update_stock$', views.UpdateStock.as_view(), name="update_stock"),
+    url(r'^shop/checkout$', never_cache(views.Checkout.as_view()), name="checkout"),
+    url(r'^shop/get_cart$', never_cache(views.get_cart), name="get_cart"),
+    url(r'^shop/update_stock$', never_cache(views.UpdateStock.as_view()), name="update_stock"),
     url(r'^admin/core/delivery/(?P<id>[0-9]+)/details/$', views.delivery_details, name="delivery_details"),
     url(r'^shop/command_successfull', TemplateView.as_view(
         template_name="core/command_successfull.html"), name="command_successfull"),
-    url(r'^shop/set_product_quantity/(?P<product>[0-9]+)/(?P<quantity>[0-9]+)/', views.set_product_quantity,
-        name="set_product_quantity"),
+    url(r'^shop/set_product_quantity/(?P<product>[0-9]+)/(?P<quantity>[0-9]+)/',
+        never_cache(views.set_product_quantity), name="set_product_quantity"),
     url(r'^requires_js$', views.RequiresJs.as_view(), name="requires_js"),
     url(r'^menu_(?P<page>[a-z_]+)/$', views.SubMenusPage.as_view(), name="menu_page"),
     url(r'^news/(?P<pk>[0-9]+)/$', views.NewsPage.as_view(), name="one_news_page"),
