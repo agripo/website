ee56621c0c22ae543af277f48eb6ca76fca24c5e
Brice PARENT <briceparent@free.fr>
2015-10-21 18:40:40 +0200

    Mail sending works - We send one when the commands are validated

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,7 @@
+import smtplib
 from django import forms
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
 
 from core.models.users import AgripoUser, CustomerData
 from core.models.shop import Product, Command, CommandProduct, Delivery
@@ -53,6 +56,7 @@ class CheckoutForm(forms.ModelForm):
         command.save()
         cart_products = Product.static_get_cart_products(self.customer)
         command.total = 0
+        command_content = ''
         for product in cart_products:
             the_product = Product.objects.get(id=product['id'])
             quantity = product['quantity']
@@ -60,7 +64,26 @@ class CheckoutForm(forms.ModelForm):
             command_product = CommandProduct(command=command, product=the_product, quantity=quantity)
             command_product.save()
             the_product.buy(quantity)
+            command_content += "<strong>{}</strong> x <strong>{} ({} FCFA)</strong><br />".format(
+                quantity, the_product.name, quantity * the_product.price)
 
+        command_content += "Total : <strong>{} FCFA</strong><br />".format(command.total)
         command.save()
         Product.static_clear_cart(self.customer)
+
+        title = 'Agripo - Commande validée'
+        html_content = 'Bonjour,<br />Votre commande a été validée avec succès.<br /><br />'
+        html_content += 'Elle sera disponible au point de rendez-vous suivant : {}<br /><br />'.format(
+            self.cleaned_data['delivery'])
+        html_content += "Contenu de votre commande : <br />{}<br /><br />".format(command_content)
+        html_content += "Nous vous remercions pour votre confiance.<br /><br />L'équipe d'Agripo"
+        content = strip_tags(html_content.replace("<br />", "\n"))
+        mail_from = 'briceparent@free.fr'
+        mail_to = ['brice@websailors.fr']
+        try:
+            send_mail(title, content, mail_from, mail_to, fail_silently=False, html_message=html_content)
+        except smtplib.SMTPException:
+            # @todo log this!!!
+            pass
+
         return command
