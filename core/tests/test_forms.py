802718367496217a81cdc7a41ab98050056d950e
Brice PARENT <briceparent@free.fr>
2015-10-28 17:32:45 +0100

    Validating some UT

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -24,27 +24,33 @@ class CheckoutFormTest(CoreTestCase):
     def test_init(self):
         CheckoutForm(customer=self.customer)  # Should not raise
 
-    def test_form_no_validation_for_blank_items(self):
+    def test_form_no_validation_for_blank_delivery(self):
         form = CheckoutForm(dict(delivery=None, message=""), customer=self.customer)
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors, {
-            'delivery': ['This field is required.'],
-        })
+        self.assertEqual(form.errors['delivery'], ['This field is required.'])
 
     def test_valid_data(self):
         self.add_products_to_cart(5)
         form = CheckoutForm({
             'delivery': self.deliveries[0].pk,
-            'message': "Some message"
+            'message': "Some message",
+            'first_name': "John",
+            'last_name': "Doe",
+            'phone': '0102030405'
         }, customer=self.customer)
         self.assertTrue(form.is_valid())
         command = form.save()
         self.assertEqual(command.delivery, self.deliveries[0])
         self.assertEqual(command.message, "Some message")
 
-    def test_blank_data(self):
+    def test_blank_data_is_invalid(self):
         form = CheckoutForm({}, customer=self.customer)
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors, {
-            'delivery': ['This field is required.'],
-        })
+
+    def test_required_fields(self):
+        form = CheckoutForm({}, customer=self.customer)
+        self.assertEquals(form.errors['first_name'], ['This field is required.'])
+        self.assertEquals(form.errors['last_name'], ['This field is required.'])
+        self.assertEquals(form.errors['phone'], ['This field is required.'])
+        self.assertEquals(form.errors['delivery'], ['This field is required.'])
+
