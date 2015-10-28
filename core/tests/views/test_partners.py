e58f14dbfd1926dc77e5dbaec3dabb0cf953fed9
Brice PARENT <briceparent@free.fr>
2015-10-28 18:28:05 +0100

    Partners are now shown with their logo

--- core/tests/views/test_partners.py
+++ core/tests/views/test_partners.py
@@ -35,7 +35,7 @@ class PartnersViewTest(PartnersBaseTestCase):
     def test_display_each_partner_s_name(self):
         partners = self._insert_random_partners(5)
         for partner in partners:
-            self._assert_partners_page_contains('<h2>{}</h2>'.format(partner.name), 1)
+            self._assert_partners_page_contains('<span class="partner_name">{}</span>'.format(partner.name), 1)
 
     def test_contains_each_partner_s_description(self):
         partners = self._insert_random_partners(5)
@@ -50,7 +50,7 @@ class PartnersViewTest(PartnersBaseTestCase):
     def test_contains_each_partner_s_logo(self):
         partners = self._insert_random_partners(5)
         for partner in partners:
-            self._assert_partners_page_contains('background-image: url(/media/{});'.format(partner.logo), 1)
+            self._assert_partners_page_contains('<img src="/media/{}"/>'.format(partner.logo), 1)
 
     def test_partners_page_displays_message_if_no_partners(self):
         self._assert_partners_page_contains('id="no_partner_to_show_message', 1)
@@ -58,16 +58,22 @@ class PartnersViewTest(PartnersBaseTestCase):
 
 class PartnersRightModuleTest(PartnersBaseTestCase):
 
-    def test_right_module_displays_block_if_there_are_partners(self):
+    def test_display_block_if_there_are_partners(self):
         self._insert_random_partners(5)
         response = self.client.get(reverse('home_page'))
         self.assertContains(response, '"partnersCarousel"')
 
-    def test_right_module_displays_all_partners(self):
+    def test_display_all_partners(self):
         self._insert_random_partners(5)
         response = self.client.get(reverse('home_page'))
         self.assertContains(response, '"carousel_one_partner_name"', 5)
 
-    def test_right_module_hides_block_if_no_partners(self):
+    def test_hide_block_if_no_partners(self):
         response = self.client.get(reverse('home_page'))
         self.assertNotContains(response, '"partnersCarousel"')
+
+    def test_contains_each_partner_s_logo(self):
+        partners = self._insert_random_partners(5)
+        response = self.client.get(reverse('home_page'))
+        for partner in partners:
+            self.assertContains(response, 'background-image: url(/media/{});'.format(partner.logo), 1)
