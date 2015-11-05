d5e19cd3b6a7e15181111da9dc2471ee7e599d23
Brice PARENT <briceparent@free.fr>
2015-11-05 18:00:09 +0100

    Enhanced the deployer script (it now lists the tags on active branch only, and forbids deploying to productoin from anything else than master

--- deploy_tools/deploy.sh
+++ deploy_tools/deploy.sh
@@ -3,10 +3,37 @@
 type="$2"
 tag="$1"
 
+list_available_tags () {
+    echo "List of all the tags on current branch : "
+    git for-each-ref --sort='*authordate' --shell | \
+    while read entry
+    do
+        it_s_a_tag=false
+        c=0
+        for word in $entry
+        do
+            if [ "$c" -eq 1 ]; then
+                if [ "$word" = "'tag'" ]; then
+                    it_s_a_tag=true
+                fi
+            elif [ "$c" -eq 2 ]; then
+                if [ "$it_s_a_tag" = true ]; then
+                    tag="${word:11:${#word}-12}"
+                    on_branch=`git branch --contains $tag | grep "*"`
+                    if [ "$on_branch" != "" ]; then
+                        echo "$tag"
+                    fi
+                fi
+            fi
+            c=$((c+1))
+        done
+    done
+}
+
 if [ -z $tag ]
     then
-        echo "You should pass a git tag as first argument to this comand. Here are the available tags : "
-        git log --tags --pretty="format:%ci %d" | grep "tag:"
+        echo "You should pass a git tag as first argument to this command. Here are the available tags : "
+        list_available_tags
         exit
 fi
 
@@ -20,6 +47,11 @@ if [ $type = "staging" ]
     then
         server="staging.agripo-website.brice.xyz"
     else
+        active_branch=`git name-rev --name-only HEAD`
+        if [ "$active_branch" != "master" ]; then
+            echo "Error! You have to be on master to push to production"
+            exit
+        fi
         server="www.agripo.net"
 fi
 
